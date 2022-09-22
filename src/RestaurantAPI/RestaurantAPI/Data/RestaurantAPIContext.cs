0ef77d03b4c1173fa452c9d712673dadd2e20463
Jon Galloway <jongalloway@gmail.com>
2022-09-22 12:52:02 -0700

    Scaffold menu item endpoints

---
+++ src/RestaurantAPI/RestaurantAPI/Data/RestaurantAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PointOfSale.Models;
+
+namespace RestaurantAPI.Data
+{
+    public class RestaurantAPIContext : DbContext
+    {
+        public RestaurantAPIContext (DbContextOptions<RestaurantAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PointOfSale.Models.Item> Item { get; set; } = default!;
+    }
+}
--- src/RestaurantAPI/RestaurantAPI/Program.cs
+++ src/RestaurantAPI/RestaurantAPI/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using RestaurantAPI.Data;
+using RestaurantAPI;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<RestaurantAPIContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RestaurantAPIContext") ?? throw new InvalidOperationException("Connection string 'RestaurantAPIContext' not found.")));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,5 +22,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapItemEndpoints();
 
 app.Run();
