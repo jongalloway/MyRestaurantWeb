126f4c31a032d9f2c0e14c9641d7e9bd594c7f73
Jon Galloway <jongalloway@gmail.com>
2022-09-22 13:30:54 -0700

    Add seed data

--- src/RestaurantAPI/RestaurantAPI/Migrations/20220922202714_SeedData.Designer.cs
+++ src/RestaurantAPI/RestaurantAPI/Migrations/20220922202714_SeedData.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestaurantAPI.Data;
 
@@ -10,9 +11,10 @@ using RestaurantAPI.Data;
 namespace RestaurantAPI.Migrations
 {
     [DbContext(typeof(RestaurantAPIContext))]
-    partial class RestaurantAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20220922202714_SeedData")]
+    partial class SeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
